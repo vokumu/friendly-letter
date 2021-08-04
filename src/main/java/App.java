65fa9023c7715781922dd3558462c37f8c11670d
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-04 15:58:35 +0300

    alot of things have been done here

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,36 +1,30 @@
-import static spark.Spark.*;
-
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.HashMap;
+import  java.util.Map;
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        get("/hello", (request, response) ->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Hello From Afar</h1>" +
-                        "<p>Dear Friend,</p>" +
-                        "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                        "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                        "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                        "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                        "<p>Cheers,</p>" +
-                        "<p>Travel Enthusiast Jane</p>" +
-                        "</body>" +
-                        "</html>"
-        );
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "hello.hbs");
+        }, new HandlebarsTemplateEngine());
 
-        get("/favorite_photos", (request, response) ->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Favorite Traveling Photos</h1>" +
-                        "<ul>" +
-                        "<li><img src='/images/2.2landscape.jpg' alt='A photo of a mountain range.'/></li>" + "<li><img src='/images/2.3water-nature-ex.jpg' alt='A photo of a swamp.'/></li>" + "</ul>" + "</body>" + "</html>" ); } }
+        get("/favorite_photos", (request, response) ->{
+            return  new ModelAndView(new HashMap(),"favorite_photos.hbs");
+                }, new HandlebarsTemplateEngine());
+        get("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/greeting_card", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String recipient = request.queryParams("recipient");
+            String sender = request.queryParams("sender");
+            model.put("recipient", recipient);
+            model.put("sender", sender);
+            return new ModelAndView(model, "greeting_card.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
